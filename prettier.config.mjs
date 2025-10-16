109fab973bb5563d27e25b088a71b7f75cbdaa29
dpletzke <dpletzke@gmail.com>
2025-10-15 23:20:09 -0500

    chore: align prettier and lint tooling across workspace

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 80,
+};
+
+export default config;
+
