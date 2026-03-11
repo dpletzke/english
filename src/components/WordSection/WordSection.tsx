da17b778ad4ec384d152ae718ff138dd08221b6e
dpletzke <dpletzke@gmail.com>
2026-03-11 00:13:29 -0500

    feat: add top app bar icons and separate CTA from header

--- src/components/WordSection/WordSection.tsx
+++ src/components/WordSection/WordSection.tsx
@@ -3,14 +3,13 @@ import styled from "styled-components";
 const WordSection = styled.section`
   display: grid;
   grid-template-rows: minmax(0, 1fr) auto auto;
-  gap: 10px;
+  gap: 20px;
   width: 100%;
   max-width: 560px;
   margin: 0 auto;
   padding-inline: clamp(2px, 1.8vw, 10px);
   justify-items: center;
   align-items: center;
-  flex: 1 1 auto;
 `;
 
 export default WordSection;
